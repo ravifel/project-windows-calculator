57ddc18bad5b78c3cc8c72ad8edef56ae0c32d3e
Ravi Silva <ravifelipee@gmail.com>
2024-12-11 08:20:47 -0300

    Project completion

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,114 @@ class Calculator {
 
     // Add digit on calculator screen
     addDigit(digit) {
+        // Check if the current operation already has a dot
+        if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+            return;
+        }
         this.currentOperation = digit;
-        this.updateScreen()
+        this.updateScreen();
+    }
+
+    // Process all calculator operations
+    processOperation(operation) {
+        // Check if current is empty
+        if (this.currentOperationText.innerText === "" && operation !== "C") {
+            // Change operation
+            if (this.previousOperationText.innerText !== "") {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+        // Get current and previous value
+        let operationValue;
+        const previous = +this.previousOperationText.innerText.split(" ")[0];
+        const current = +this.currentOperationText.innerText;
+
+        switch (operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "-":
+                operationValue = previous - current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "*":
+                operationValue = previous * current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "/":
+                operationValue = previous / current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "DEL":
+                this.processDelOperator();
+                break;
+            case "CE":
+                this.processClearCurrentOperation();
+                break;
+            case "C":
+                this.processClearAllOperation();
+                break;
+            case "=":
+                this.processEqualsOperator();
+                break;
+            default:
+                return;
+        }
     }
 
     // Change values of the calculator screen
-    updateScreen() {
-        this.currentOperationText.innerText += this.currentOperation;
+    updateScreen(
+        operationValue = null,
+        operation = null,
+        current = null,
+        previous = null
+    ) {
+        console.log(operationValue, operation, current, previous)
+        if (operationValue === null) {
+            this.currentOperationText.innerText += this.currentOperation;
+        } else {
+            // Check if the value is zero, if so, just add the current value
+            if (previous === 0) {
+                operationValue = current;
+            }
+            // Add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = "";
+        }
+    }
+
+    // Change Math Operation
+    changeOperation(operation) {
+        const mathOperation = ["/", "*", "-", "+"];
+        if (!mathOperation.includes(operation)) {
+            return
+        }
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // Delete the last digit
+    processDelOperator() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    // Clear Current Operation
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = "";
+    }
+
+    // Clear All Operation
+    processClearAllOperation() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+
+    // Process an Operator
+    processEqualsOperator() {
+        const operation = previousOperationText.innerHTML.split(" ")[1];
+        this.processOperation(operation);
     }
 }
 
@@ -37,9 +138,9 @@ buttons.forEach((btn) => {
             // Try to convert the received value to a number
             console.log(value);
             calc.addDigit(value);
-
         } else {
             console.log("Operation: " + value);
+            calc.processOperation(value);
         }
     })
 })
